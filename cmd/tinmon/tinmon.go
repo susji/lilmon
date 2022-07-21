1ffde5604039c24af4c417cd11ad9f658b622e0e
susji <susji@protonmail.com>
2022-07-21 21:51:36 +0300

    Specify a few test metrics and measure them once

--- cmd/tinmon/tinmon.go
+++ cmd/tinmon/tinmon.go
@@ -4,9 +4,16 @@ import (
 	"database/sql"
 	"flag"
 	"log"
+	"os/exec"
+	"strings"
 
 	_ "github.com/glebarez/go-sqlite"
 )
+
+type metric struct {
+	command     string
+	description string
+}
 
 func db_init(db_path string) *sql.DB {
 	db, err := sql.Open("sqlite", db_path)
@@ -23,10 +30,27 @@ func db_init(db_path string) *sql.DB {
 	return db
 }
 
+func run_metrics(shell string, metrics []*metric) error {
+	for n, m := range metrics {
+		log.Printf("Running command %d/%d: %q\n", n+1, len(metrics), m.command)
+		cmd := exec.Command(shell, "-c", m.command)
+		out, err := cmd.Output()
+		if err != nil {
+			log.Println("... run failed: ", err)
+			continue
+		}
+		cleaned := strings.TrimSpace(string(out))
+		log.Printf("... run worked and returned: %q\n", cleaned)
+	}
+	return nil
+}
+
 func main() {
 	var db_path string
+	var shell string
 
 	flag.StringVar(&db_path, "db-path", "/tmp/tinmon.sqlite", "Path where to store tinmon database file")
+	flag.StringVar(&shell, "shell", "/bin/sh", "Shell to invoke when obtaining metrics")
 	flag.Parse()
 
 	db := db_init(db_path)
@@ -35,5 +59,18 @@ func main() {
 			log.Println("warning: error when closing database: ", err)
 		}
 	}()
+
+	metrics := []*metric{
+		&metric{
+			command:     "find /tmp/ -type f | wc -l",
+			description: "count of files under /tmp",
+		},
+		&metric{
+			command:     "vm_stat |fgrep 'Pages active:'|cut -d ':' -f 2|cut -d '.' -f1",
+			description: "pages of memory in use",
+		},
+	}
+
+	run_metrics(shell, metrics)
 
 }
